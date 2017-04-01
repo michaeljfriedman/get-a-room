34b3d187e6e08bdc8759cdfd0d3c64e691daa0ff
Michael J Friedman <mfred240@gmail.com>
2017-04-01 03:48:26 -0400

    Added a view for the landing page - simple "hello world"
    - Includes the view handler function in get_a_room/views.py
      and a test in get_a_room/tests.py to make sure it works. Passes
      the test.

--- get_a_room/get_a_room_app/tests.py
+++ get_a_room/get_a_room_app/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class IndexViewTests(TestCase):
+    '''
+    Tests for the index (landing) page
+    '''
+
+    def test_index_page_loads_hello_world(self):
+        '''
+        Tests that the index page loads with content "Hello world!"
+        '''
+        response = self.client.get(reverse('get_a_room_app:index'))
+        self.assertEqual(response.content, 'Hello world!')
