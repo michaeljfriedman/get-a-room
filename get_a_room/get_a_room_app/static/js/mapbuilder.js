23c4c4d342d7f78d3d1366b81b9240cedd098599
Michael J Friedman <mfred240@gmail.com>
2017-04-09 20:48:01 -0400

    Reorganized mapbuilder.js for better readability
    - Mostly just commented each section so it's easier to
      understand what's going on.

--- get_a_room/get_a_room_app/static/js/mapbuilder.js
+++ get_a_room/get_a_room_app/static/js/mapbuilder.js
@@ -1,38 +1,47 @@
-// Domain name for AJAX requests
-var DOMAIN = 'http://localhost:8000/';
+/*
+ * mapbuilder.js
+ *
+ * This script loads, populates, and defines the behavior of the map
+ * interface.
+ */
 
-// Helper function for replacing all occurrences of spaces with hyphens
-// in building name
-String.prototype.replaceAll = function(search, replacement) {
-    var target = this;
-    return target.split(search).join(replacement);
-};
+$(document).ready(function() {
 
-// Initialize the map
-var map = L.map('map').setView([40.345129502014764, -74.65826869010927], 17);
-map.setMaxZoom(17).setMinZoom(16);
-//map.setMaxBounds([[40.33761, -74.67769], [40.350697, -74.64053]]);
+    // Domain name for AJAX requests
+    var DOMAIN = 'http://localhost:8000/';
 
-L.control.locate({options:{
-    setView: 'untilPan',
-    icon: 'icon-location',
-    }}).addTo(map);
-// L.Control.extend();
+    // Helper function for replacing all occurrences of spaces with hyphens
+    // in building name
+    String.prototype.replaceAll = function(search, replacement) {
+        var target = this;
+        return target.split(search).join(replacement);
+    };
 
-layers = {}
-// load a tile layer
-layers.tiles = L.tileLayer('https://api.mapbox.com/styles/v1/bnprks/cizxah1p6003n2rs6zu65xd7i/tiles/256/{z}/{x}/{y}?access_token={accessToken}',
-{
-  accessToken:'pk.eyJ1IjoiYm5wcmtzIiwiYSI6ImNqMHVpaHBndjA2NG0zMnFheG5kbG5wa3AifQ.Cypl8hCriRSkA4XF-4GgMQ',
-  attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
-  id: 'bnprks.9754e7af',
-  maxZoom: 17,
-  minZoom: 9
-}).addTo(map);
+    // Initialize the map
+    var map = L.map('map').setView([40.345129502014764, -74.65826869010927], 17);
+    map.setMaxZoom(17).setMinZoom(16);
+    //map.setMaxBounds([[40.33761, -74.67769], [40.350697, -74.64053]]);
 
-/*----------------------------------------------------------------------------*/
+    L.control.locate({options:{
+        setView: 'untilPan',
+        icon: 'icon-location',
+        }}).addTo(map);
+    // L.Control.extend();
 
-// Pull locations and their GPS coordinates from the database, store in 'places'
+    // Load a tile layer
+    layers = {}
+    layers.tiles = L.tileLayer('https://api.mapbox.com/styles/v1/bnprks/cizxah1p6003n2rs6zu65xd7i/tiles/256/{z}/{x}/{y}?access_token={accessToken}',
+    {
+      accessToken:'pk.eyJ1IjoiYm5wcmtzIiwiYSI6ImNqMHVpaHBndjA2NG0zMnFheG5kbG5wa3AifQ.Cypl8hCriRSkA4XF-4GgMQ',
+      attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
+      id: 'bnprks.9754e7af',
+      maxZoom: 17,
+      minZoom: 9
+    }).addTo(map);
+
+    /*------------------------------------------------------------------------*/
+
+    // Pull locations and their GPS coordinates from the database, store in 'places'
 
     var places = {
         "type": "FeatureCollection",
@@ -73,128 +82,130 @@ layers.tiles = L.tileLayer('https://api.mapbox.com/styles/v1/bnprks/cizxah1p6003
        }
    });
 
-/*----------------------------------------------------------------------------*/
+    /*------------------------------------------------------------------------*/
 
-// Create and define behavior of markers
+    // Create and define behavior of markers
 
-function onEachFeature(feature, layer) {
-    var popupContent = "<p>Click for stats on rooms in this building!</p> <strong>" + feature.properties.popupContent + "</strong>";
+    // On mouse hover
+    function onSetHover(e) {
+        var marker = e.target;
+        marker.setStyle({
+            weight: 5,
+            color: '#666',
+            dashArray: '',
+            fillOpacity: 0.7
+        });
+        this.openPopup();
+    }
 
+    // On removing mouse hover
+    function onRemoveHover(e) {
+        var marker = e.target;
+        layers.places.resetStyle(marker);
+        this.closePopup();
+    }
 
-    layer.bindPopup(popupContent,{closeButton: false, autoPan: false});
-    layer.on({
-            'mouseover': setHover.bind(layer),
-            'mouseout': removeHover.bind(layer),
-             'click': function(e) {
-                map.closePopup();
-                var building = feature.properties.popupContent;
-                openSidePanel(building);
-                // map.addLayer(layers.tiles).addLayer(layers.places);
-            }
-    });
-}
+    // Loads stats for 'building', and opens the side panel with those stats
+    function openSidePanel(building) {
+        // map.removeLayer(layers.tiles).removeLayer(layers.places);
 
-function setHover(e) {
-    this.openPopup()
-    var layer = e.target;
-    layer.setStyle({
-        weight: 5,
-        color: '#666',
-        dashArray: '',
-        fillOpacity: 0.7
-    });
-}
+        // Make AJAX request to get building stats
+        $.ajax({
+            url: DOMAIN + 'stats/building/' + building.replaceAll(' ', '-').toLowerCase(),
+            success: function(result) {
+                // Parse JSON response and populate view
+                var buildingStats = JSON.parse(result);
+                if (!jQuery.isEmptyObject(buildingStats)) {
+                    $('#building-name').text(buildingStats.name);
+                    for (i = 0; i < buildingStats.rooms.length; i++) {
+                        // Make row for this room's stats
+                        var room = buildingStats.rooms[i];
+                        $('table#building-stats').append('<tr id="building-stats-row' + i + '"></tr>');
+                        $('tr#building-stats-row' + i).append('<td id="building-stats-room-number' + i + '"></td>');
+                        $('tr#building-stats-row' + i).append('<td id="building-stats-room-occupancy' + i + '"></td>');
 
-// Loads stats for 'building', and opens the side panel with those stats
-function openSidePanel(building) {
-    // map.removeLayer(layers.tiles).removeLayer(layers.places);
-
-    // Make AJAX request to get building stats
-    $.ajax({
-        url: DOMAIN + 'stats/building/' + building.replaceAll(' ', '-').toLowerCase(),
-        success: function(result) {
-            // Parse JSON response and populate view
-            var buildingStats = JSON.parse(result);
-            if (!jQuery.isEmptyObject(buildingStats)) {
-                $('#building-name').text(buildingStats.name);
-                for (i = 0; i < buildingStats.rooms.length; i++) {
-                    // Make row for this room's stats
-                    var room = buildingStats.rooms[i];
-                    $('table#building-stats').append('<tr id="building-stats-row' + i + '"></tr>');
-                    $('tr#building-stats-row' + i).append('<td id="building-stats-room-number' + i + '"></td>');
-                    $('tr#building-stats-row' + i).append('<td id="building-stats-room-occupancy' + i + '"></td>');
-
-                    // Populate row with data
-                    $('td#building-stats-room-number' + i).text(room.number);
-                    $('td#building-stats-room-occupancy' + i).text(room.occupancy + ' / ' + room.capacity);
+                        // Populate row with data
+                        $('td#building-stats-room-number' + i).text(room.number);
+                        $('td#building-stats-room-occupancy' + i).text(room.occupancy + ' / ' + room.capacity);
+                    }
+                } else {
+                    // Show error message in side panel instead of room data
+                    $('#building-name').text('Oops!');
+                    $('#building-stats-error-message').text('We couldn\'t find stats for this room!');
                 }
-            } else {
-                // Show error message in side panel instead of room data
-                $('#building-name').text('Oops!');
-                $('#building-stats-error-message').text('We couldn\'t find stats for this room!');
+
+                // Slide out the panel with the content
+                $('#side-panel').slideDown(250);
             }
-
-            // Slide out the panel with the content
-            $('#side-panel').slideDown(250);
-        }
-    });
-}
-
-// Closes the side panel
-function closeSidePanel() {
-    $('#side-panel').slideUp(250);
-
-    // Clear contents
-    $('table#building-stats').html('');
-    $('#building-stats-error-message').text('');
-}
-
-function removeHover(e) {
-    map.closePopup();
-    layers.places.resetStyle(e.target);
-}
-
-layers.places = L.geoJSON(places, {
-    style: function (feature) {
-        return feature.properties && feature.properties.style;
-    },
-
-    onEachFeature: onEachFeature,
-
-    pointToLayer: function (feature, latlng) {
-        var rooms = feature.properties.extra.rooms;
-
-        // Set marker fill color based on occupancy of the rooms
-        var FILLCOLOR;
-        var numRoomsOpen = 0;
-        var totalRooms = rooms.length;
-        for (i = 0; i < totalRooms; i++) {
-            if (rooms[i].occupancy / rooms[i].capacity < 0.8) {
-                numRoomsOpen++;
-            }
-        }
-        var ratio = numRoomsOpen / totalRooms;
-        if (ratio >= 0.5)
-            FILLCOLOR = '#36ce4c'; // green
-        else if (0.0 < ratio && ratio < 0.5)
-            FILLCOLOR = '#fec041'; // yellow
-        else
-            FILLCOLOR = '#fc635d'; // red
-
-        return L.circleMarker(latlng, {
-            radius: 8,
-            fillColor: FILLCOLOR,
-            color: '#000',
-            weight: 0.2,
-            opacity: 0.5,
-            fillOpacity: 0.3
         });
     }
-}).addTo(map);
 
-/*----------------------------------------------------------------------------*/
+    // Closes the side panel
+    function closeSidePanel() {
+        $('#side-panel').slideUp(250);
 
-$(document).ready(function() {
+        // Clear contents
+        $('table#building-stats').html('');
+        $('#building-stats-error-message').text('');
+    }
+
+    // Place markers on map
+    layers.places = L.geoJSON(places, {
+        style: function (feature) {
+            return feature.properties && feature.properties.style;
+        },
+
+        onEachFeature: function (feature, layer) {
+            // Adds mouse hover/click listeners and sets the marker's popup
+            // window content. The parameter 'feature' passed in is one of the
+            // feature objects in 'places', defined in the last section.
+            var popupContent = "<p>Click for stats on rooms in this building!</p> <strong>" + feature.properties.popupContent + "</strong>";
+            layer.bindPopup(popupContent,{closeButton: false, autoPan: false});
+            layer.on({
+                'mouseover': onSetHover.bind(layer),
+                'mouseout': onRemoveHover.bind(layer),
+                'click': function(e) {
+                    this.closePopup();
+                    var building = feature.properties.popupContent;
+                    openSidePanel(building);
+                    // map.addLayer(layers.tiles).addLayer(layers.places);
+                }
+            });
+        },
+
+        pointToLayer: function (feature, latlng) {
+            // Set this marker fill color based on occupancy of the rooms.
+            // Again, 'feature' is one of the feature objects in 'places'.
+            var rooms = feature.properties.extra.rooms;
+            var FILLCOLOR;
+            var numRoomsOpen = 0;
+            var totalRooms = rooms.length;
+            for (i = 0; i < totalRooms; i++) {
+                if (rooms[i].occupancy / rooms[i].capacity < 0.8) {
+                    numRoomsOpen++;
+                }
+            }
+            var ratio = numRoomsOpen / totalRooms;
+            if (ratio >= 0.5)
+                FILLCOLOR = '#36ce4c'; // green
+            else if (0.0 < ratio && ratio < 0.5)
+                FILLCOLOR = '#fec041'; // yellow
+            else
+                FILLCOLOR = '#fc635d'; // red
+
+            return L.circleMarker(latlng, {
+                radius: 8,
+                fillColor: FILLCOLOR,
+                color: '#000',
+                weight: 0.2,
+                opacity: 0.5,
+                fillOpacity: 0.3
+            });
+        }
+    }).addTo(map);
+
+    /*------------------------------------------------------------------------*/
+
     // Set click listener on 'close' button of the side panel
     $('#close-side-panel').click(closeSidePanel);
 });
